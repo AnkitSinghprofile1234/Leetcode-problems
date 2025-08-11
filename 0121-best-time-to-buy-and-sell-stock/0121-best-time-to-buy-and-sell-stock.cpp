a390b0493ad43a2d2f222612cb0f9f68635e6e50
Kumar Ankit <ankitsingh987654dpsrnc@gmail.com>
2025-08-11 19:54:56 +0530

    Time: 0 ms (100%), Space: 97.5 MB (7.07%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+int maxProfit(vector<int>& prices) {
+    int minPrice = INT_MAX; //buy ka sabse kam price
+    int maxProfit = 0;      // best profit
+
+     for (int price : prices) { //prices ke har element ko ek-ek karke price variable me daalte hai, aur loop ke andar use karo.
+        if (price < minPrice) {
+         minPrice = price;
+     }
+      else {
+    int profit = price - minPrice;
+    if (profit > maxProfit) 
+     maxProfit = profit;
+                }
+            }
+        return maxProfit ;
+    }
+};
