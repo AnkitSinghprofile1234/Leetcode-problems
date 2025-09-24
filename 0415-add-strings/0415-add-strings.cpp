1dd24e92b5e1c6a2db24b059c21fce122f6b8fd0
Kumar Ankit <ankitsingh987654dpsrnc@gmail.com>
2025-09-24 18:50:13 +0530

    Time: 0 ms (100%), Space: 9.5 MB (32.93%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -1,23 +1,43 @@
 class Solution {
 public:
-    string addStrings(string num1, string num2) {
-       int i = num1.size() - 1, j = num2.size() - 1, carry = 0;
-    string result = "";
-    
-    while(i >= 0 || j >= 0 || carry) {
-        int n1 = i >= 0 ? num1[i--] - '0' : 0;
-        int n2 = j >= 0 ? num2[j--] - '0' : 0;
-        int sum = n1 + n2 + carry;
-        carry = sum / 10;
-        result = char(sum % 10 + '0') + result;
-    }
-    
-    return result;
-}
 
-int main() {
-    string num1 = "456", num2 = "77";
-    cout << addStrings(num1, num2) << endl;
-    return 0; 
+    string add(string num1, string num2) {
+        string ans;  // used for storing the final ans
+        int index1 = num1.size()-1, index2 = num2.size()-1;  //as we add two numbers from backwards.
+        int sum, carry = 0;
+
+        while(index2>=0){
+            sum = (num1[index1]-'0') + (num2[index2]-'0') + carry;
+            carry = sum/10;
+            char c = '0' + sum%10;
+            ans += c;
+            index2--,index1--;
+        }
+
+        // index1>=0
+       // agar num1 ke extra digits bache hain
+
+        while(index1>=0){
+            sum = (num1[index1]-'0') + carry;
+            carry = sum/10;
+            char c = '0' + sum%10;
+            ans += c;
+           index1--;
+        }
+
+        if(carry){
+            ans += '1';
+        }
+        reverse(ans.begin(),ans.end());
+            return ans;
+    }
+
+    string addStrings(string num1, string num2){
+        if(num1.size() < num2.size()){
+            return add(num2,num1);
+        }
+        else{
+            return add(num1,num2);
+        }
     }
 };
