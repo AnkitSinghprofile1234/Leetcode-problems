412f82e7cf785e32cb280147c87593502282f516
Kumar Ankit <ankitsingh987654dpsrnc@gmail.com>
2025-10-01 21:29:47 +0530

    Time: 0 ms (100%), Space: 8.6 MB (44.53%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        
-        long binform = n; // long isliye taki value bhot bda na ho eg 2^30 bhot bda value hai jo int me store nhi hoga.
-
-        if(binform < 0){ // agar exponent negative hai
-            x = 1/x;  // base ka reciprocal le liye.
-            binform = -binform; //here i have stored binform as the power  eg. 2^3 so here 3 is binform.
+        long binform = n; 
+        if(binform < 0){
+            x = 1/x; 
+            binform = -binform;
         }
         double ans = 1;
 
         while(binform > 0){
-            if(binform %2 == 1){ // agar last bit 1 hai
-                ans *= x; // Multiplication ka neutral element hota hai 1 (jaise addition ka neutral element 0 hota hai).
+            if(binform %2 == 1){ 
+                ans *= x; 
             }
             x *= x;
             binform /= 2;
