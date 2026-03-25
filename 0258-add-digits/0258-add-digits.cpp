185b80a80f2d4cdde0b7a7d0518146635fbcc11d
Kumar Ankit <ankitsingh987654dpsrnc@gmail.com>
2026-03-25 12:09:08 +0530

    Time: 0 ms (100%), Space: 8.7 MB (7.13%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,7 +1,13 @@
 class Solution {
 public:
     int addDigits(int num) {
-        if (num == 0) return 0;
-        return 1 + (num - 1) % 9;
+        if(num < 10){
+            return num;
+        }
+        int d = num % 10;
+         num = num/10;
+        int ans = addDigits(num);
+        int j =  d + ans;
+        return addDigits(j);
     }
 };
