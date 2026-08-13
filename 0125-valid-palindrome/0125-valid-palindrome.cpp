e540ba3ab86aabb32849ed0b56288475d46e19ab
Kumar Ankit <ankitsingh987654dpsrnc@gmail.com>
2026-08-13 16:58:40 +0530

    Time: 3 ms (28.64%), Space: 9.8 MB (94.42%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-  bool check(string &s,int low,int high){
-    if(low >= high) return true;
-
-    // we will skip the , and space between the string for the left side of the string
-    if(!isalnum(s[low])){
-    return check(s,low+1,high);
-    }
-
- // we will skip the , and space between the string for the right side of the string
-      if(!isalnum(s[high])) return check(s,low,high-1);
-
-  if(tolower(s[low]) != tolower(s[high])){
-    return false;
-  }
-    return check(s,low+1,high-1);
-
-  }
     bool isPalindrome(string s) {
-        return check(s, 0, s.length() - 1);
+        int left = 0;
+        int right = s.length()-1;
+        while(left < right){
+
+            while(left<right && !isalnum(s[left])){
+                left++;
+            }
+            while(left<right && !isalnum(s[right])){
+                right--;
+            }
+            if(tolower(s[left]) != tolower(s[right])){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 };
